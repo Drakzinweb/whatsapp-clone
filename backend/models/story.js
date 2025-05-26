51cfeaff55284fc6606849ae569afbee5b9c037b
drak <santanadavi247@gmail.com>
2025-05-26 07:26:51 -0300

    Adiciona modelo Story

--- backend/models/story.js
+++ backend/models/story.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
 
 const storySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  media: { type: String, required: true },
-  type: { type: String, enum: ['image', 'video'], required: true },
-  viewedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  createdAt: { type: Date, default: Date.now },
-  expiresAt: { type: Date, default: () => new Date(Date.now() + 24 * 60 * 60 * 1000) }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  media: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['image', 'video'],
+    required: true
+  },
+  viewedBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  expiresAt: {
+    type: Date,
+    default: () => new Date(Date.now() + 24 * 60 * 60 * 1000) // 24h
+  }
 }, {
   timestamps: true
 });
